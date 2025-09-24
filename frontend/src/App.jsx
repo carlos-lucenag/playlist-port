4d840303a80459450ef7798d281ef6e1a2da5163
carlos-lucenag <carlos.henriche.goncalves@ccc.ufcg.edu.br>
2025-09-24 15:54:03 -0300

    feat(frontend): complete frontend redesign

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -83,93 +83,96 @@ function App() {
   };
 
   return (
-    <>
-      <div className="h-screen bg-slate-400 flex flex-col justify-center items-center text-center">
-        <div id="navbar" className="mb-10">
-          <h1 className="mb-3 text-5xl">Playlist Port</h1>
-          <p className="text-xl">
-            Transfer your playlists through streaming services.
-          </p>
-        </div>
-        <button
-          onClick={handleScrollToTransfer}
-          className="cursor-pointer mt-14 text-3xl justify-self-center bg-slate-300 border w-fit h-fit px-6 py-3 rounded-2xl hover:bg-slate-200"
-        >
-          Start
-        </button>
+    <div>
+      
+      <div id="navbar" className="mt-28 mb-28 text-center">
+        <h1 className="text-8xl mb-10 text-neutral-50 tracking-wide">Playlist Port</h1>
+        <p className="text-3xl text-neutral-200">
+          Transfer your playlists through streaming services.
+        </p>
       </div>
 
-      <form
-        ref={transferSectionRef}
-        onSubmit={handleSubmit}
-        className="h-screen bg-slate-500 flex flex-col justify-center items-center text-center"
-      >
-        <p>Transfer from</p>
-        <select
-          name="origin"
-          id="origin-service"
-          required
-          value={formData.origin}
-          onChange={handleChange}
-          className="mt-3 text-center bg-slate-400 p-2 rounded"
-        >
-          <option value="" disabled>
-            Select
-          </option>
-          <option value="spotify">Spotify</option>
-          <option value="youtube">Youtube</option>
-        </select>
-        <p className="mt-6">To</p>
-        <select
-          name="destination"
-          id="destination-service"
-          required
-          value={formData.destination}
-          onChange={handleChange}
-          className="mt-3 text-center bg-slate-400 p-2 rounded"
-        >
-          <option value="" disabled>
-            Select
-          </option>
-          <option value="spotify">Spotify</option>
-          <option value="youtube">Youtube</option>
-        </select>
-        <input
-          type="text"
-          name="playlistUrl"
-          placeholder="Paste your playlist's URL"
-          className="bg-slate-200 p-2 rounded-2xl mt-5"
-          value={formData.playlistUrl}
-          onChange={handleChange}
-          required
-        />
-        <button
-          type="submit"
-          disabled={isLoading}
-          className="mt-6 bg-slate-300 px-4 py-2 rounded-lg hover:bg-slate-200"
-        >
-          {isLoading ? "Transferring..." : "Transfer"}
-        </button>
-        {transferResult && (
-          <div className="mt-4 p-2 bg-green-200 text-green-800 rounded">
-            <p>Success! Your new playlist is here:</p>
-            <a
-              href={transferResult.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="font-bold underline"
-            >
-              {transferResult.url}
-            </a>
+      <div className="place-self-center rounded-4xl w-5xl flex justify-between items-center">
+        <div className="bg-neutral-900 h-[22rem] w-[26rem] rounded-4xl flex flex-col items-center justify-evenly text-neutral-100 shadow-lg">
+          <p className="text-4xl text-center">Select origin<br></br>and destination</p>
+          <div className="w-xs p-2">
+            <div className="flex items-center justify-evenly mb-10">
+              <p className="text-2xl w-20 text-center">From</p>
+              <select
+                name="origin"
+                id="origin-service"
+                required
+                value={formData.origin}
+                onChange={handleChange}
+                className="text-center text-xl bg-neutral-700 text-neutral-500 p-2 rounded-full"
+              >
+                <option value="" disabled>
+                  Select
+                </option>
+                <option value="spotify">Spotify</option>
+                <option value="youtube">Youtube</option>
+              </select>
+            </div>
+            <div className="flex items-center justify-evenly">
+              <p className="text-2xl w-20 text-center">To</p>
+              <select
+                name="origin"
+                id="origin-service"
+                required
+                value={formData.origin}
+                onChange={handleChange}
+                className="text-center text-xl bg-neutral-700 text-neutral-500 p-2 rounded-full"
+              >
+                <option value="" disabled>
+                  Select
+                </option>
+                <option value="spotify">Spotify</option>
+                <option value="youtube">Youtube</option>
+              </select>
+            </div>
           </div>
-        )}
-        {error && (
-          <div className="mt-4 p-2 bg-red-200 text-red-800 rounded">
-            <p>Error: {error}</p>
-          </div>
-        )}
-      </form>
-    </>
+        </div>
+
+        <div className="bg-neutral-900 h-[22rem] w-[26rem] rounded-4xl flex flex-col items-center justify-evenly text-neutral-100 shadow-lg">
+          <p className="text-4xl text-center w-xs">Paste your<br></br>playlist URL</p>
+          <input
+            type="text"
+            name="playlistUrl"
+            placeholder="Paste your playlist's URL"
+            className="bg-neutral-700 text-md py-2 px-4 rounded-full w-xs"
+            value={formData.playlistUrl}
+            onChange={handleChange}
+            required
+          />
+
+          <button
+            type="submit"
+            disabled={isLoading}
+            className="text-xl w-30 bg-linear-to-r from-[#40BF40] to-[#7266CC] p-2 rounded-full"
+          >
+            {isLoading ? "Transferring..." : "Transfer"}
+          </button>
+          {transferResult && (
+            <div className="mt-4 p-2 bg-green-200 text-green-800 rounded">
+              <p>Success! Your new playlist is here:</p>
+              <a
+                href={transferResult.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-bold underline"
+              >
+                {transferResult.url}
+              </a>
+            </div>
+          )}
+          {error && (
+            <div className="mt-4 p-2 bg-red-200 text-red-800 rounded">
+              <p>Error: {error}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
 
