2c0060b4b5bca7c3a874aee9462ff8455f94e170
carlos-lucenag <carlos.henriche.goncalves@ccc.ufcg.edu.br>
2025-10-05 23:08:54 -0300

    chore: atualizando espacamento no frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,30 +15,65 @@ function App() {
   };
 
   return (
-    <div className="relative w-screen h-screen flex flex-col justify-start overflow-x-hidden">
-      <div id="navbar" className="text-[#181C17] mt-40 ml-[16%] w-fit">
-        <h1 className="text-8xl text-shadow-md font-bold tracking-wide mb-6">
-          Playlist Port
-        </h1>
-        <p className="text-3xl text-shadow-xs font-medium text-[#636B61]">
-          Transfer your playlists through streaming services.
-        </p>
+    <div className="max-w-[60rem] flex flex-col">
+      <div className="flex flex-col h-dvh">
+        <div id="navbar" className="mt-30">
+          <h1 className="text-[#181C17] text-8xl text-shadow-lg font-bold tracking-wide">
+            Playlist Port
+          </h1>
+          <p className="text-[#636B61] mt-6 text-3xl font-medium">
+            Transfer your playlists through streaming services.
+          </p>
+        </div>
+
+        <button
+          onClick={handleScroll}
+          className="
+            border-2 border-[#FAFAF9] 
+            text-[#395D28] text-5xl font-medium tracking-wide
+            bg-linear-to-r from-[#F0F6EE] to-[#99F53D] 
+            w-fit 
+            self-center
+            mt-[30vh]
+            px-10 py-3 
+            rounded-full 
+            shadow-[inset_0_-2px_4px_rgba(0,0,0,0.25),inset_0_4px_8px_rgba(255,255,255,0.3),0_2px_8px_rgba(0,0,0,0.20)] 
+            transform transition-all duration-200 ease-in-out 
+            hover:-translate-y-0.5"
+        >
+          Start
+        </button>
       </div>
 
-      <button
-        onClick={handleScroll}
-        className="border-2 border-[#FAFAF9] text-[#395D28] bg-linear-to-r from-[#F0F6EE] to-[#99F53D] w-fit px-10 py-3 rounded-full text-5xl self-center mt-72 font-medium tracking-wide shadow-[inset_0_-2px_4px_rgba(0,0,0,0.25),inset_0_4px_8px_rgba(255,255,255,0.3),0_2px_8px_rgba(0,0,0,0.20)] mb-64 transform transition-all duration-200 ease-in-out hover:-translate-y-0.5"
-      >
-        Start
-      </button>
+      <img
+        src={greenEllipse}
+        className="absolute w-screen self-center top-90 h-lvh -z-10"
+      />
 
-      <img src={greenEllipse} className="absolute top-80 w-full h-lvh -z-10" />
-
-      <section ref={connectPageRef}>
+      <section className="h-dvh" ref={connectPageRef}>
+        <svg
+          width="100vw"
+          height="600"
+          className="absolute left-0 right-0 -z-10"
+        >
+          <defs>
+            <linearGradient id="Gradient1" x1={0} x2={0} y1={1} y2={0}>
+              <stop offset="0%" stopColor="#fafaf9" />
+              <stop offset="100%" stopColor="#fafaf990" />
+            </linearGradient>
+          </defs>
+          <rect
+            width="100vw"
+            height="600"
+            fill="url(#Gradient1)"
+            rx="10"
+            ry="10"
+          />
+        </svg>
         <Connect />
       </section>
 
-      <section ref={transferPageRef}>
+      <section className="border h-dvh" ref={transferPageRef}>
         <Transfer />
       </section>
     </div>
--- frontend/src/components/Connect.jsx
+++ frontend/src/components/Connect.jsx
@@ -61,37 +61,51 @@ function Connect() {
   const handleConnectYoutube = () => {
     window.location.href = "http://localhost:3000/login/youtube";
   };
-  
-  return (
-    <div>
-      <div className="py-40 relative bottom-0 w-screen flex flex-col">
-        <div className="text-[#181C17] ml-[16%] w-fit">
-          <h1 className="text-6xl text-shadow-md font-bold tracking-wide mb-5">
-            1. Connect
-          </h1>
-          <p className="text-3xl italic text-shadow-xs text-[#636B61]">
-            To your accounts
-          </p>
-        </div>
 
-        <div className="grid mt-36 gap-5 self-center text-3xl text-[#395D28]">
-          <button
-            className={`border-2 border-[#FAFAF9] bg-linear-to-r from-[#F0F6EE03] to-[#D9FBB6] w-48 px-4 py-2 rounded-full text-3xl self-center font-medium tracking-wide shadow-[inset_0_-2px_4px_rgba(0,0,0,0.1),inset_0_4px_8px_rgba(255,255,255,0.3),0_2px_4px_rgba(0,0,0,0.1)] transform transition-all duration-200 ease-in-out hover:-translate-y-0.5 ${
+  return (
+    <div className="mt-60 flex flex-col">
+      <h1 className="text-6xl text-[#181C17] text-shadow-md font-bold tracking-wide">
+        1. Connect
+      </h1>
+      <p className="mt-5 text-3xl text-[#636B61] italic">To your accounts</p>
+
+      <div className="grid mt-[20vh] gap-5 self-center text-3xl text-[#395D28]">
+        <button
+          className={`
+            border-2 border-[#FAFAF9] 
+            bg-linear-to-r from-[#F0F6EE03] to-[#D9FBB6] 
+            w-48 
+            px-4 py-2 
+            rounded-full 
+            text-3xl font-medium tracking-wide 
+            self-center 
+            shadow-[inset_0_-2px_4px_rgba(0,0,0,0.1),inset_0_4px_8px_rgba(255,255,255,0.3),0_2px_4px_rgba(0,0,0,0.1)] 
+            transform transition-all duration-200 ease-in-out 
+            hover:-translate-y-0.5 ${
               isConnectedYoutube ? " border-[#99F53D]" : "bg-[#F0F6EE]"
             }`}
-            onClick={handleConnectYoutube}
-          >
-            Youtube
-          </button>
-          <button
-            className={`border-2 border-[#FAFAF9] bg-linear-to-r from-[#F0F6EE03] to-[#D9FBB6] w-48 px-4 py-2 rounded-full text-3xl self-center font-medium tracking-wide shadow-[inset_0_-2px_4px_rgba(0,0,0,0.1),inset_0_4px_8px_rgba(255,255,255,0.3),0_2px_4px_rgba(0,0,0,0.1)] transform transition-all duration-200 ease-in-out hover:-translate-y-0.5 ${
+          onClick={handleConnectYoutube}
+        >
+          Youtube
+        </button>
+        <button
+          className={`
+            border-2 border-[#FAFAF9] 
+            bg-linear-to-r from-[#F0F6EE03] to-[#D9FBB6] 
+            w-48 
+            px-4 py-2 
+            rounded-full 
+            text-3xl font-medium tracking-wide 
+            self-center 
+            shadow-[inset_0_-2px_4px_rgba(0,0,0,0.1),inset_0_4px_8px_rgba(255,255,255,0.3),0_2px_4px_rgba(0,0,0,0.1)] 
+            transform transition-all duration-200 ease-in-out 
+            hover:-translate-y-0.5 ${
               isConnectedSpotify ? "border-[#99F53D]" : "bg-[#F0F6EE]"
             }`}
-            onClick={handleConnectSpotify}
-          >
-            Spotify
-          </button>
-        </div>
+          onClick={handleConnectSpotify}
+        >
+          Spotify
+        </button>
       </div>
     </div>
   );
